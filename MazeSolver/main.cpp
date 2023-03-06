6582aac125c8e0a62eef4ca81d29cd8076801963
Jason G <42660969+jsongi@users.noreply.github.com>
2023-03-05 23:25:34 -0800

    Amendment to first commit

--- MazeSolver/main.cpp
+++ MazeSolver/main.cpp
@@ -5,29 +5,163 @@
 #include <random>
 #include <vector>
 #include <utility>
+#include <stack>
 
 //This implementation of a maze generating algorithm uses a randomized version of Prim's algorithm
 //   https://en.wikipedia.org/wiki/Maze_generation_algorithm
 
-void generateMaze();
-void printMenu();
+//Dimensions of the board (needs to be odd numbers)
+const int row = 27;
+const int col = 61;
 
+void generateMaze(char(&maze)[row][col]);
+void solveMazeDFS(int, int, char(&maze)[row][col], bool(&visited)[row][col]);
 
+struct Cell {
+	int row;
+	int col;
+	bool visited;
+};
+
+// Define the four possible moves from a cell
+const int numMoves = 4;
+const int moves[numMoves][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
+
+// Function to check if a cell is within the maze bounds and is unvisited
+bool isValidMove(int curRow, int curCol, char (&maze)[row][col], bool (&visited)[row][col]) {
+	return curRow >= 1 && curRow < (row - 1) && curCol >= 1 && curCol < (col - 1) && (maze[curRow][curCol] == ' ' || maze[curRow][curCol] == 'e') && !visited[curRow][curCol];
+}
 
 int main()
 {
-	generateMaze();
+	char maze[row][col];
+	bool visited[row][col] = { };
+
+	generateMaze(maze);
+	solveMazeDFS((row - 2), 1, maze, visited);
+
 	return 0;
 }
 
-void generateMaze()
-{
-	//Dimensions of the board (needs to be odd numbers)
-	const int row = 7;
-	const int col = 11;
+// Function to perform a depth-first search (DFS) to find a path through the maze
+void solveMazeDFS(int startRow, int startCol, char(&maze)[row][col], bool(&visited)[row][col]) {
+	//Starts at the end and solves the maze backwards
+	//Checks for valid moves
+	//Marks valid unvisited cells from the currently visited cell as moves from that cell
+	//When we reach start, we have a path
+	char solved[row][col];
+	char displaySolved[row][col];
 
-	//Board
-	char maze[row][col];
+	for (int rows = 0; rows < row; rows++)
+	{
+		for (int cols = 0; cols < col; cols++)
+		{
+			solved[rows][cols] = maze[rows][cols];
+			displaySolved[rows][cols] = maze[rows][cols];
+		}
+	}
+
+	// Create a stack to keep track of cells to visit
+	std::stack<Cell> s;
+
+	int curRow;
+	int curCol;
+	int lastRow;
+	int lastCol;
+
+	// Mark the starting cell as visited and push it onto the stack
+	Cell startCell = { startRow, startCol };
+	visited[startRow][startCol] = true;
+	s.push(startCell);
+
+	// Loop until the stack is empty
+	while (!s.empty()) {
+		// Pop a cell from the stack
+		Cell currCell = s.top();
+		lastRow = currCell.row;
+		lastCol = currCell.col;
+		s.pop();
+
+		// If the current cell is the end of the maze, return true
+		if (currCell.row == 1 && currCell.col == col - 2) {
+			
+			curRow = 1;
+			curCol = col - 2;
+
+			while (displaySolved[curRow][curCol] != 'p')
+			{
+				if (solved[curRow][curCol] == '<')
+				{
+					curCol -= 1;
+					if (displaySolved[curRow][curCol] != 'p')
+						displaySolved[curRow][curCol] = '.';
+				}
+				else if (solved[curRow][curCol] == '>')
+				{
+					curCol += 1;
+					if (displaySolved[curRow][curCol] != 'p')
+						displaySolved[curRow][curCol] = '.';
+				}
+				else if (solved[curRow][curCol] == '^')
+				{
+					curRow -= 1;
+					if (displaySolved[curRow][curCol] != 'p')
+						displaySolved[curRow][curCol] = '.';
+				}
+				else
+				{
+					curRow += 1;
+					if (displaySolved[curRow][curCol] != 'p')
+						displaySolved[curRow][curCol] = '.';
+				}
+			}
+			for (int rows = 0; rows < row; rows++)
+			{
+				for (int cols = 0; cols < col; cols++)
+				{
+					std::cout << displaySolved[rows][cols];
+				}
+				std::cout << std::endl;
+			}
+			return;
+		}
+
+		// Loop through the four possible moves from the current cell
+		for (int i = 0; i < numMoves; i++) {
+			int row = currCell.row + moves[i][0];
+			int col = currCell.col + moves[i][1];
+
+			// If the move is valid, mark the cell as visited and push it onto the stack
+			if (isValidMove(row, col, maze, visited)) {
+				visited[row][col] = true;
+				if (lastRow < row && lastCol == col)
+				{
+					solved[row][col] = '^';
+				}
+				else if (lastRow > row && lastCol == col)
+				{
+					solved[row][col] = 'v';
+				}
+				else if (lastRow == row && lastCol > col)
+				{
+					solved[row][col] = '>';
+				}
+				else
+				{
+					solved[row][col] = '<';
+				}
+				Cell nextCell = { row, col };
+				s.push(nextCell);
+			}
+		}
+	}
+
+	// If no path was found, return
+	return;
+}
+
+void generateMaze(char(&maze)[row][col])
+{
 
 	//Holds coordinates for walls to check
 	std::vector<std::pair<int, int>> wallList;
